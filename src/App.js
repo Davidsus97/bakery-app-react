eddbc6f617bb120aa6fd6b12b26ab29c2d7b1247
Davidsus97 <davidsus97@gmail.com>
2023-08-21 23:18:52 -0400

    change all

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import React from "react";
+import logo from './{'/logo2.gpj'}';
+import './App.css';
+
+
+
+
 
 
 
 function App() {
   
+  const numbers = [1, 2, 3, 4, 5];
+  const listItems = numbers.map((number) =>
+  <li>{number}</li>
+);
+  
 
   return (
     <div>
       <header>
-        <button>
-        Refresh data
-        </button>
+      <img src={logo} width={100} height={100} />
       </header>
       <body>
       Holidays
       </body>
-      
+      <button>
+        Refresh data
+        </button>
+      <ul>{listItems}</ul>
     </div>
 
   );
